9f492bc4635a17e93ca62c6a6516115c446ee10d
Sanat Kumar Gupta <123228827+SKG24@users.noreply.github.com>
2024-11-04 16:08:14 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL  # Initialize cached inverse as NULL
+    set <- function(y) {
+        x <<- y  # Assign new matrix
+        inv <<- NULL  # Reset cached inverse
+    }
+    get <- function() x  # Return the current matrix
+    setInverse <- function(inverse) inv <<- inverse  # Cache the inverse
+    getInverse <- function() inv  # Retrieve cached inverse
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()  # Check for cached inverse
+        if(!is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)  # Return cached inverse if available
+        }
+        data <- x$get()  # Get the current matrix
+        inv <- solve(data, ...)  # Compute inverse
+        x$setInverse(inv)  # Cache the computed inverse
+        inv
 }
